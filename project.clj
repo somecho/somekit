f42831df4ea55fe1266847c00397eba6c26a6f14
samuel cho <samonthepiano@gmail.com>
2022-12-29 09:51:02 +0100

    version 0.1.0

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject org.clojars.some/somekit "0.1.0"
+  :description "Somé's Clojure(Script) creative coding toolkit"
+  :url "https://github.com/somecho/somekit"
+
+  :dependencies
+  [[org.clojure/clojurescript "1.11.60" :scope "provided"]]
+
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :repositories
+  {"clojars" {:url "https://clojars.org/repo"
+              :sign-releases false}}
+
+  :source-paths
+  ["src/main"])
